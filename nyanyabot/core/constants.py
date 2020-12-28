9b791b206d17617eaff2fec3e70d0a607a7a0661
Andreas Bielawski <brawl@nyanya.de>
2020-12-28 17:01:01 +0100

    Remove unused code

--- nyanyabot/core/constants.py
+++ nyanyabot/core/constants.py
@@ -6,6 +6,5 @@ class Constants:
     This class holds often used constants.
     """
 
-    USER_ROLES = ["creator", "administrator", "member"]
     UTC_TIMEZONE = pytz.utc
     GERMAN_TIMEZONE = pytz.timezone("Europe/Berlin")
