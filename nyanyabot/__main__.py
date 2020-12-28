0d6dc8daa02554ff2ef5ac365f3cfcaccd6fd742
Andreas <brawl@nyanya.de>
2020-12-28 17:02:27 +0100

    Implement core (#8)

--- nyanyabot/__main__.py
+++ nyanyabot/__main__.py
@@ -1,5 +1,25 @@
+import argparse
+
+from nyanyabot.core.configuration import Configuration
+from nyanyabot.core.nyanyabot import NyaNyaBot
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Plugin-based Telegram bot")
+    parser.add_argument(
+            dest="configpath",
+            type=str,
+            metavar="<config.json>",
+            help="Path to config.json"
+    )
+    return parser.parse_args()
+
+
 def main() -> None:
-    print("Hello world!")
+    args = parse_arguments()
+    config = Configuration(args.configpath)
+    bot = NyaNyaBot(config)
+    bot.start()
 
 
 if __name__ == "__main__":
---
+++ nyanyabot/core/constants.py
@@ -0,0 +1,10 @@
+import pytz
+
+
+class Constants:
+    """
+    This class holds often used constants.
+    """
+
+    UTC_TIMEZONE = pytz.utc
+    GERMAN_TIMEZONE = pytz.timezone("Europe/Berlin")
